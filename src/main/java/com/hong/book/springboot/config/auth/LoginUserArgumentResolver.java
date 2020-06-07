60b9971213fc39ccabaa2b1059fd3ceacfa402ad
asrig9 <cmychs@gmail.com>
2020-06-07 21:26:54 +0900

    Oauth2 - 구글 / 네이버 로그인

---
+++ src/main/java/com/hong/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.hong.book.springboot.config.auth;
+
+import com.hong.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    // 컨트롤러 메서드의 특정 파라미터를 지원하는지를 판단. 여기서는 파라미터에 @LoginUser가 붙어있고 파라미터 클래스타입 SessionUser.clss인 경우 true를 반환.
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    // resolveArgument() : 파라미터에 전달한 객체를 생성한다. 여기서는 세션에서 객체를 가져온다.
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/hong/book/springboot/web/IndexController.java
+++ src/main/java/com/hong/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hong.book.springboot.web;
 
+import com.hong.book.springboot.config.auth.LoginUser;
 import com.hong.book.springboot.config.auth.dto.SessionUser;
 import com.hong.book.springboot.service.posts.PostsService;
 import com.hong.book.springboot.web.dto.PostsResponseDto;
@@ -23,17 +24,17 @@ public class IndexController {
     @GetMapping("/")
     // model :: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
             // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달한다.
-    public String index(Model model){
+    // @LoginUser 어노테이션 추가 :: 기존에 (User)HttpSession.getAttribute("user")로 가져오던 세션정보값을 개선.
+    public String index(Model model, @LoginUser SessionUser user) {
         // mustache starter로 자동적으로 앞의 경로는 src/main/resources/templates로, mustache파일로 변환됨.
         model.addAttribute("posts", postsService.findAllDesc());
         // 앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 유저를 저장하도록 구성.
         // 로그인 성공 시, getAttribute로 값을 가져올 수 있다.
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         // 세션에 저장된 값이 있을 때만 model에 userName으로 등록한다.
-        if(user != null){
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
-
         return "index";
     }
 

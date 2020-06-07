f265ae60712de5eaf8afb8a562d59339c6792a50
asrig9 <cmychs@gmail.com>
2020-06-07 21:43:13 +0900

    Oauth2 - 구글 / 네이버 로그인

--- src/main/java/com/hong/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/hong/book/springboot/config/auth/dto/SessionUser.java
@@ -3,9 +3,11 @@ package com.hong.book.springboot.config.auth.dto;
 import com.hong.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
 // 인증된 사용자 정보만 필요하다. 그 외에 필요한 정보들은 없으니 name, email, picture만 필드로 선언.
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
